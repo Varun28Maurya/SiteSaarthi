3ecaf9a70d660f0a8f61e4fa0a073b2af4601d4e
Prakash23114 <prakashmandal23114@gmail.com>
2025-12-24 22:10:49 +0530

    Refactor Progress component with updated syntax

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -1,8 +1,6 @@
 "use client"
-
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
-
 import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
@@ -21,3 +19,4 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
+
