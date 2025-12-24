283525bbae6298ce6668fe6b17a55c5523f09bbb
Prakash23114 <prakashmandal23114@gmail.com>
2025-12-24 22:06:03 +0530

    Refactor button component for improved clarity

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
-
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
@@ -46,3 +45,4 @@ const Button = React.forwardRef(({ className, variant, size, asChild = false, ..
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+
